robur-coop/conex

commit 2b45b5038efc9f68c00b0855cb58dfbbe1b80d24

conex to/of_string: directly output 0x (preserve to_string (of_string X) = x)

src/conex_opam_encoding.ml
@@ -27,7 +27,7 @@ let rec encode_s x =
       | Wire.List l -> OpamParserTypes.List (np (List.map encode_s l))
       | Wire.Identifier i -> OpamParserTypes.Ident i
       | Wire.Data s -> OpamParserTypes.String s
-      | Wire.Bigint i -> OpamParserTypes.Ident ("0x" ^ Uint.to_string i)
+      | Wire.Bigint i -> OpamParserTypes.Ident (Uint.to_string i)
       | Wire.Smallint i -> OpamParserTypes.Int i
       | Wire.Pair (i, s) -> OpamParserTypes.Group (np [ encode_s i ; encode_s s ])
       | Wire.And (a, b) -> OpamParserTypes.Logop (np `And, encode_s a, encode_s b)

test/test_conex.ml
@@ -313,7 +313,7 @@ module ExprTests = struct
       | (`SHA256, h), e -> h, e
     in
     Alcotest.(check string "hash is the new one" "123456" hash);
-    Alcotest.(check string "epoch is the new one" "1" (Uint.to_string epoch))
+    Alcotest.(check string "epoch is the new one" "0x1" (Uint.to_string epoch))
 
     let basic_eval_tests () =
       let str = "expr: (0 [])" in

src/conex_resource.ml
@@ -36,7 +36,7 @@ module Wire = struct
     | Or of s * s
 
   let rec s_to_string = function
-    | Bigint x -> "0x" ^ Uint.to_string x
+    | Bigint x -> Uint.to_string x
     | Smallint i -> string_of_int i
     | Data s -> string_of_int (String.length s) ^ "'" ^ s
     | Identifier i -> i

src/conex_utils.ml
@@ -147,14 +147,14 @@ module Uint = struct
     else
       (false, Int64.succ x)
 
-  let to_string s = Printf.sprintf "%LX" s
+  let to_string s = Printf.sprintf "0x%LX" s
 
   let pp ppf i = Format.pp_print_string ppf (to_string i) [@@coverage off]
 
   let decimal s = Printf.sprintf "%Lu" s
 
   let of_string s =
-    try Some (Int64.of_string ("0x" ^ s)) with Failure _ -> None
+    try Some (Int64.of_string s) with Failure _ -> None
 
   let of_float f =
     if f < 0. then

src/conex_utils.mli
@@ -149,7 +149,7 @@ module Uint : sig
       [next] is always the next integer in the [Z/2^64-1Z] group. *)
   val succ : t -> bool * t
 
-  (** [to_string t] is [t] converted to a string in hexadecimal ([%LX]).  *)
+  (** [to_string t] is [t] converted to a string in hexadecimal ([0x%LX]).  *)
   val to_string : t -> string
 
   (** [pp] is a pretty printer *)
@@ -159,7 +159,7 @@ module Uint : sig
   val decimal : t -> string
 
   (** [of_string s] attempts to parse the string [s] as hexadecimal encoded
-      number using [Int64.of_string "0x" ^ s]. *)
+      number using [Int64.of_string s]. *)
   val of_string : string -> t option
 
   (** [of_float f] is [Int64.of_float f] if [f >= 0.0]. *)

test/test_uint.ml
@@ -9,9 +9,9 @@ let ui =
   (module M: Alcotest.TESTABLE with type t = M.t)
 
 let the x = match Uint.of_string x with Some x -> x | None -> Alcotest.fail "cannot parse"
-let max = the "FFFFFFFFFFFFFFFF"
-let max_int64 = the "7FFFFFFFFFFFFFFF"
-let min_int64 = the "8000000000000000"
+let max = the "0xFFFFFFFFFFFFFFFF"
+let max_int64 = the "0x7FFFFFFFFFFFFFFF"
+let min_int64 = the "0x8000000000000000"
 
 let compare_initial () =
   Alcotest.(check int "compare 0 0 is 0"
@@ -43,24 +43,21 @@ let succ_initial () =
 let r () =
   let buf = Bytes.create 16 in
   let one i =
-    let r = Random.int 16 in
+    let r =
+      let r = Random.int 16 in
+      if i = 0 && r = 0 then r + 1 else r
+    in
     let ascii = r + (if r < 10 then 0x30 else 0x37) in
     Bytes.set buf i (char_of_int ascii)
   in
   for i = 0 to 15 do one i done ;
-  Bytes.to_string buf
-
-let rec trim0 s =
-  if String.get s 0 = '0' then
-    trim0 (String.slice ~start:1 s)
-  else
-    s
+  "0x" ^ Bytes.to_string buf
 
 let to_of_str_id n () =
   for _i = 0 to n do
     let r = r () in
     Alcotest.(check string "to_of_string is identity"
-                (trim0 r) Uint.(to_string (the r)))
+                r Uint.(to_string (the r)))
   done
 
 let compare_random n () =

commit 4c24d18ba932c2e10dcc544de9ce3356a8138cf4

fix bigint decoding (broken since 2b45b5038efc9f68c00b0855cb58dfbbe1b80d24)
also add a test which includes a bigint

src/conex_opam_encoding.ml
@@ -48,8 +48,8 @@ let rec decode_s s =
   match s.OpamParserTypes.pelem with
   | OpamParserTypes.Ident data ->
     if String.is_prefix ~prefix:"0x" data then
-      match Uint.of_string (String.slice ~start:2 data) with
-      | None -> Error "cannot parse unsigned integer"
+      match Uint.of_string data with
+      | None -> Error ("cannot parse unsigned integer: " ^ data)
       | Some x -> Ok (Wire.Bigint x)
     else if data = "emptymap" then
       Ok (Wire.Map M.empty)

test/test_conex.ml
@@ -994,6 +994,9 @@ module VF = Conex_verify.Make(
   end)
 
 module RepoTests = struct
+  let target_t =
+    Alcotest.testable Target.pp Target.equal
+
   let ctree =
     let module M = struct
       type t = (Digest.t * Uint.t * S.t) Tree.t
@@ -1010,6 +1013,27 @@ module RepoTests = struct
         Tree.equal eq_triple
     end in
     (module M : Alcotest.TESTABLE with type t = M.t)
+
+  let target_dec () =
+    let data = {|expr: [
+        [size 0x1FF]
+        [filename "foo/foo.1.0.0/opam"]
+        [
+          digest
+          [
+            "sha256=ccea69740e61ce074ed53019ac0dcde3c9602b00bdf21b12c0e53be2328f9b52"
+          ]
+        ]
+      ]|}
+    in
+    let target = Target.{
+        filename = [ "foo" ; "foo.1.0.0" ; "opam" ];
+        digest = [ `SHA256, "ccea69740e61ce074ed53019ac0dcde3c9602b00bdf21b12c0e53be2328f9b52" ];
+        size = Option.get (Uint.of_int 511)
+      }
+    in
+    Alcotest.check (result target_t Alcotest.string) "basic target decoding"
+      (Ok target) (Target.of_wire (wire_s data))
 
   let basic_good_target () =
     let id1 = "foo"
@@ -1794,6 +1818,7 @@ module RepoTests = struct
          keys [ "bar" ] expr [ targets1 ; targets2 ])
 
   let tests = [
+    "decoding a target", `Quick, target_dec ;
     "basic good target", `Quick, basic_good_target ;
     "partial good targets", `Quick, partial_good_targets ;
     "wrong epoch target", `Quick, wrong_epoch_target ;